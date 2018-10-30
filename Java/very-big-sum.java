89f2e1bca5f4f280c73e29d8b771e11b1f959453
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-10-30 13:54:33 -0400

    Add Problem Description

--- Java/very-big-sum.java
+++ Java/very-big-sum.java
@@ -1,3 +1,13 @@
+// Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+//
+// Function Description -------
+//
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+//
+// aVeryBigSum has the following parameter(s):
+//
+// ar: an array of integers .
+
 public class Solution {
 
     // Complete the aVeryBigSum function below.
